deaeec55f4213f55bffe4f2ede7aa5d27d4b8eb1
aajour <ahmed.ajourr@gmail.com>
2017-09-19 17:14:50 +0300

    use prepared statements relates #16

--- src/model/queries/skills.js
+++ src/model/queries/skills.js
@@ -1,27 +1,25 @@
 const connection = require('./../database/dbConnection');
 const addSkill = (Obj, cb) => {
 
-    connection.query(`INSERT INTO skills (skill,skillvalue,user_id) VALUES ('${Obj.skill}',${Obj.skillvalue},${Obj.user_id})`, (err, res) => {
+    connection.query(`INSERT INTO skills (skill,skillvalue,user_id) VALUES ($1,$2,$3)`, (err, res) => {
         if (err) {
             cb(err);
         } else {
             cb(null, res);
         }
-    });
+    }, [Obj.skill, Obj.skillvalue, Obj.user_id]);
 };
 
 const updateSkill = (Obj, cb) => {
-    const sql = {
-        text: `UPDATE skills SET skillvalue=${Obj.skillvalue} WHERE skill='${Obj.skill}' AND user_id=${Obj.user_id}`
-    };
 
-    connection.query(sql, (err, res) => {
+
+    connection.query(`UPDATE skills SET skillvalue=$1 WHERE skill=$2 AND user_id=$3`, (err, res) => {
         if (err) {
             cb(err);
         } else {
             cb(null, res);
         }
-    });
+    }, [Obj.skillvalue, Obj.skill, Obj.user_id]);
 };
 
 module.exports = {
