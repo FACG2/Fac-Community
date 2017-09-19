00557be60eaa3fb41f0def86217ad4c2cf2b2d8b
Abdallah <you@example.com>
2017-09-19 10:54:18 +0300

    edit package.json scripts

--- src/app.js
+++ src/app.js
@@ -23,12 +23,12 @@ app.set('port', process.env.PORT || 4000);
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(controllers);
 
-app.use((err, req, res, next)=>{
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.render('error', {
-    message:err.message,
-    error:{}
-  })
-})
+    message: err.message,
+    error: {}
+  });
+});
 
 module.exports = app;
