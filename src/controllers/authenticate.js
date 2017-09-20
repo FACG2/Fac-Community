95dfe2b8b4421cac93b92603a9c92e57805205c9
Abdallah <you@example.com>
2017-09-20 11:06:54 +0300

    get private orgnazations relates #14

--- src/controllers/authenticate.js
+++ src/controllers/authenticate.js
@@ -4,9 +4,9 @@ exports.get = (req, res) => {
   github.fetchToken(req.query.code, (accessToken) => {
     github.getResource('user', accessToken, (err, user) => {
       if (err) {
-        console.log('error', err);
+        console.log(err);
       } else {
-        console.log('not error', user);
+        console.log(user);
       }
       res.end();
     });
