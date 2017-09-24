68af8bff2f07658e694e25d8659912cf0b8feece
Abdallah <you@example.com>
2017-09-24 09:45:45 +0300

    move middlewares to a folder

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const exphbs = require('express-handlebars');
 const controllers = require('./controllers/index.js');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const errMiddleware = require('./middlewares/error.js');
+const authRoutes = require('./middlewares/authenticateRoutes.js');
+console.log(authRoutes);
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -21,8 +24,9 @@ app.engine(
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser('secret'));
-app.set('port', process.env.PORT || 3000);
+app.use(errMiddleware);
+app.use(authRoutes);
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(controllers);
-
+app.set('port', process.env.PORT || 3000);
 module.exports = app;
