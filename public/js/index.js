5507325cae6d53483e88a44dd118fc95fdd1f132
Abdallah <abdallah.halees@gmail.com>
2017-09-25 15:07:43 +0300

    add front end functions relates #42

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,34 @@
+var username = document.cookie.split('username=')[1];
+var fullname = document.getElementById('name-input');
+var email = document.getElementById('email-input');
+var bio = document.getElementById('bio-input');
+var userForm = document.getElementById('update-form');
+
+get(`/basicinfo?username=${username}`, (response) => { //eslint-disable-line
+  fullname.value = response.name;
+  email.value = response.email;
+  bio.value = response.bio;
+});
+
 var counter = 1;
 var limit = 100;
-function addInput(divName){
-     if (counter == limit)  {
-          alert("You have reached the limit of adding " + counter + " inputs");
-     }
-     else {
-          var newdiv = document.createElement('div');
-          newdiv.innerHTML = "<div class='skill'><div class='skills col-6'><label class='col-form-label'>skill"+(counter +1)+"</label><br><input type='text' class='form-control' name='skill'></div><div class='values col-6'><label class='col-form-label'>Skill value</label><div  class='SkillsValue'><input class='form-control' name='skillvalue' type='number' value=''></div></div></div>";
-          document.getElementById(divName).appendChild(newdiv);
-          counter++;
-     }
+function addInput (divName) {
+  if (counter === limit) {
+    alert('You have reached the limit of adding ' + counter + ' inputs');
+  } else {
+    var newdiv = document.createElement('div');
+    newdiv.innerHTML = "<div class='skill'><div class='skills col-6'><label class='col-form-label'>skill" + (counter + 1) + "</label><br><input type='text' class='form-control' name='skill'></div><div class='values col-6'><label class='col-form-label'>Skill value</label><div  class='SkillsValue'><input class='form-control' name='skillvalue' type='number' value=''></div></div></div>";
+    document.getElementById(divName).appendChild(newdiv);
+    counter++;
+  }
 }
 
+function submitForms () {
+  var skillData = {skill: 'js',
+    skillvalue: 5,
+    username: username};
+  post('/skills', {data:'your data'}, (res) => { //eslint-disable-line
+    console.log(res);
+  });
+  // userForm.submit();
+}
