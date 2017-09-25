11dd133b128e2f0c98820cb383b5780d7a1dc66b
aajour <ahmed.ajourr@gmail.com>
2017-09-25 13:36:38 +0300

    home page

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -2,7 +2,7 @@
 exports.get = (req, res, next) => {
   const username = req.signedCookies.username;
   if (username) {
-    res.render('home', {user: username});
+    res.render('home', {user: username, title: 'Home', cssPath: '/css/home.css'});
   } else {
     res.render('login', {title: 'Home', cssPath: '/css/login.css'});
   }
