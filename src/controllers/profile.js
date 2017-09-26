7b052fd0196d248e9047f081d4c6b1e1e43e2699
mahmoudalwadia <mahmoud.alwadia@gmail.com>
2017-09-26 14:13:53 +0300

    profile info format

--- src/controllers/profile.js
+++ src/controllers/profile.js
@@ -9,7 +9,7 @@ exports.getUserInfo = (req, res, next) => {
         if (err) {
           next();
         } else {
-          console.log('hellllllllllllo', info);
+          console.log('hellllllllllllo', takeAnObject(info));
           res.render('profile.hbs', {users: info,
             username: info[0].username,
             title: 'profile',
@@ -23,3 +23,29 @@ exports.getUserInfo = (req, res, next) => {
     }
   });
 };
+
+function takeAnObject (arr) {
+  var ourObj = {};
+  ourObj.name = arr[0].name;
+  ourObj.email = arr[0].email;
+  ourObj.bio = arr[0].bio;
+  ourObj.campus = arr[0].campus;
+  ourObj.cohortnum = arr[0].cohortnum;
+  ourObj.email = arr[0].email;
+
+  var skills = arr.reduce((accum, item) => {
+    accum[item.skill] = item.skillvalue;
+    return accum;
+  }, {});
+
+  ourObj.skills = Object.keys(skills).map(skill => ({ skill, skillvalue: skills[skill] }));
+
+  var accounts = arr.reduce((accum, item) => {
+    accum[item.socail_network] = item.link;
+    return accum;
+  }, {});
+
+  ourObj.accounts = Object.keys(accounts).map(account => ({ account, accountLink: accounts[account]}));
+
+  return ourObj;
+}
