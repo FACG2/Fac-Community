bed4962b235682861b0a1ac59cffbe9457720ef2
Abdallah <abdallah.halees@gmail.com>
2017-09-26 09:51:05 +0300

    edit results.hbs relates #53

--- src/controllers/search.js
+++ src/controllers/search.js
@@ -1,11 +1,11 @@
 const searchFunctions = require('./../model/queries/search.js');
 
 exports.getAll = (req, res, next) => {
-  searchFunctions.searchAll(req.body.searchInput, (err, result) => {
+  searchFunctions.searchAll(req.query.searchInput, (err, result) => {
     if (err) {
       next(err);
     } else {
-      res.render('searchResults.hbs', {activePage: { searchResults: true }, users: result[0]});
+      res.render('results', {users: result[0]});
     }
   });
 };
