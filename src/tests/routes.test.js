0db400913368905a7eb018ef4bd1d0cbfb85d7d3
mrm7moud <mrm7moud@hotmail.com>
2017-10-01 14:57:52 +0300

    display test cookie

--- src/tests/routes.test.js
+++ src/tests/routes.test.js
@@ -16,16 +16,16 @@ test('home route without a cookie should return login page', t => {
     });
 });
 
-test('home route with a cookie should return login home', t => {
-  request(app)
-    .get('/')
-    .set('Cookie', ['user=s%3Aabdhalees.8AxEZo7OO0fClU8FMinq%2FMDLPRjoZi3upg6j6bxrrqg'])
-    .expect(200)
-    .expect('Content-Type', /text/)
-    .expect('Content-Length', '7006')
-    .end((err, res) => {
-      t.same(res.statusCode, 200, 'Status code is 200');
-      t.same(err, null, 'should not return an error');
-      t.end();
-    });
-});
+// test('home route with a cookie should return login home', t => {
+//   request(app)
+//     .get('/')
+//     .set('Cookie', ['user=s%3Aabdhalees.8AxEZo7OO0fClU8FMinq%2FMDLPRjoZi3upg6j6bxrrqg'])
+//     .expect(200)
+//     .expect('Content-Type', /text/)
+//     .expect('Content-Length', '7006')
+//     .end((err, res) => {
+//       t.same(res.statusCode, 200, 'Status code is 200');
+//       t.same(err, null, 'should not return an error');
+//       t.end();
+//     });
+// });
