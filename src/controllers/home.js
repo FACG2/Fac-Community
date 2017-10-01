9a2a16b52843add31242792bf783c9c9795a5b14
mrm7moud <mrm7moud@hotmail.com>
2017-10-01 10:54:12 +0300

    send object in the render for title and style

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -2,8 +2,19 @@
 exports.get = (req, res, next) => {
   const username = req.signedCookies.user;
   if (username) {
-    res.render('home', {user: username, title: 'Home', cssPath: '/css/home.css'});
+    res.render('home',
+      {
+        user: username,
+        title: 'Home',
+        cssPath: '/css/home.css',
+        cssPath2: '/css/search.css',
+        home: 'home'
+      });
   } else {
-    res.render('login', {title: 'Home', cssPath: '/css/login.css'});
+    res.render('login',
+      {
+        title: 'Home',
+        cssPath: '/css/login.css'
+      });
   }
 };
